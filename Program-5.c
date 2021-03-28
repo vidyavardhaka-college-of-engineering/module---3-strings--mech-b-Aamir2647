cb32de28d599c275d0f4f1f2659e4a550b5cd2b1
Aamir2647 <20me0083@vvce.ac.in>
2021-03-28 08:21:33 +0000

    5.c

--- Program-5.c
+++ Program-5.c
@@ -13,13 +13,16 @@ int main()
   int c , count = 0;
   char s[1000];
 
-  printf("Enter a string: ");
+  printf("Enter the string: ");
   scanf("%s", s);
 
   for(c=0;c<500;c++)
   {
                                //place logic to find the vowel
-  if(s[c]=='a'||s[c]=='A'||s[c]=='e'||s[c]=='E'||s[c]=='i'||s[c]=='I'||s[c]=='o'||s[c]=='O'||s[c]=='u'||s[c]=='U')
+  if(s[c]=='a'||s[c]=='A'||s[c]
+  =='e'||s[c]=='E'||s[c]=='i'||s[c]
+  =='I'||s[c]=='o'||s[c]=='O'||s[c]
+  =='u'||s[c]=='U')
   {
     count++;
   }
